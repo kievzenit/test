6e20343b85532deec7d36ff164c80482989b291a
kievzenit <kir.backender@gmail.com>
2022-10-11 14:39:54 +0300

    test solution created

---
+++ TestClassLib/TestClass.cs
@@ -0,0 +1,14 @@
+﻿namespace TestClassLib;
+
+public class TestClass
+{
+    public int Add(int a, int b)
+    {
+        if (a > b)
+        {
+            return -1;
+        }
+
+        return a + b;
+    }
+}
