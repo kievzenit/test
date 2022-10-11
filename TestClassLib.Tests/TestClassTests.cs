2a288d9592eb8c675efc51ad78494707bd5b4c88
kievzenit <contact@kievzenit.tk>
2022-10-11 15:03:38 +0300

    Tests added

--- TestClassLib.Tests/TestClassTests.cs
+++ TestClassLib.Tests/TestClassTests.cs
@@ -2,9 +2,19 @@ namespace TestClassLib.Tests;
 
 public class TestClassTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(1, 6, 7)]
+    [InlineData(6, -98, 92)]
+    [InlineData(5, 7, 12)]
+    public void Test_AddMethod(int a, int b, int expected)
     {
+        // arrange
+        var testClass = new TestClass();
 
+        // act
+        var result = testClass.Add(a, b);
+
+        // assert
+        result.Should().Be(expected);
     }
 }
